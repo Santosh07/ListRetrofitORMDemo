e0b1f315215c401e13777b0371353d3b97c9e473
Santosh Dhakal <santosh.dhakal07@gmail.com>
2018-08-29 17:06:30 +0545

    added retrofit and downloaded data

--- app/src/main/java/com/santoshdhakal/internshipchallenge/SplashActivity.java
+++ app/src/main/java/com/santoshdhakal/internshipchallenge/SplashActivity.java
@@ -1,5 +1,6 @@
 package com.santoshdhakal.internshipchallenge;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -7,12 +8,46 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import com.santoshdhakal.internshipchallenge.models.PostModel;
+import com.santoshdhakal.internshipchallenge.network.RetrofitClientInstance;
+import com.santoshdhakal.internshipchallenge.services.GetDataService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        GetDataService service = RetrofitClientInstance.getRetrofitInstance().create(GetDataService.class);
+        Call<List<PostModel>> call = service.getAllPosts();
+        call.enqueue(new Callback<List<PostModel>>() {
+            @Override
+            public void onResponse(Call<List<PostModel>> call, Response<List<PostModel>> response) {
+                ArrayList<PostModel> postLists = (ArrayList<PostModel>) response.body();
+
+                System.out.println(postLists.size());
+
+                for (PostModel model: postLists) {
+                    System.out.println("Id : " + model.getId());
+                }
+
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+
+            @Override
+            public void onFailure(Call<List<PostModel>> call, Throwable t) {
+
+            }
+        });
     }
 
 }
---
+++ app/src/main/java/com/santoshdhakal/internshipchallenge/services/GetDataService.java
@@ -0,0 +1,13 @@
+package com.santoshdhakal.internshipchallenge.services;
+
+import com.santoshdhakal.internshipchallenge.models.PostModel;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface GetDataService {
+    @GET("/posts")
+    Call<List<PostModel>> getAllPosts();
+}
