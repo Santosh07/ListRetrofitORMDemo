ffb19f592343c7066ac209023330a558f7a2221c
Santosh Dhakal <santosh.dhakal07@gmail.com>
2018-09-05 13:00:47 +0545

    fixed dimensions for list of post

--- app/src/main/java/com/santoshdhakal/internshipchallenge/SplashActivity.java
+++ app/src/main/java/com/santoshdhakal/internshipchallenge/SplashActivity.java
@@ -22,7 +22,6 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
 
         HomeViewModel homeViewModel = ViewModelProviders.of(this).get(HomeViewModel.class);
 
